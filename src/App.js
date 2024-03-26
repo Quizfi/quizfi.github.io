4fc3e05ba9c377085b703904fcb73fbcfbe965fc
jjumang <jjumang@naver.com>
2024-03-26 16:30:31 +0900

    rebuild web

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Home/HomePage'; // HomePage 컴포넌트의 경로에 맞게 조정하세요.
 import QuizGame from './components/QuizGame'; // QuizGame 컴포넌트의 경로에 맞게 조정하세요.
 
-
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} exact />
-          <Route path="/game" element={<QuizGame />} />
-        </Routes>
-      </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/game" element={<QuizGame />} />
+      </Routes>
     </Router>
   );
 }
-
 
 export default App;
