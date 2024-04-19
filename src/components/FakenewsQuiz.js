1e0f9097c75f4e39847c626e2472c82182b95dae
Jeom <163089786+Quizfi@users.noreply.github.com>
2024-04-20 02:02:25 +0900

    정답/오답로직&데이타 추가

--- src/components/FakenewsQuiz.js
+++ src/components/FakenewsQuiz.js
@@ -1,16 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './FakenewsQuiz.css';
 import quizData from '../db/fakenewsData.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const FakenewsQuiz = () => {
-    const [gameStarted, setGameStarted] = useState(false);
+    const [currentNewsIndex, setCurrentNewsIndex] = useState(0);
     const [currentNews, setCurrentNews] = useState({});
+    const [isAnswerShown, setIsAnswerShown] = useState(false);
+    const [answerMessage, setAnswerMessage] = useState('');
     const navigate = useNavigate();
 
-    const startGame = () => {
-        setGameStarted(true);
-        setCurrentNews(quizData[0]);
+    // 페이지 로드 시 자동으로 게임 시작
+    useEffect(() => {
+        setCurrentNews(quizData[currentNewsIndex]);
+    }, [currentNewsIndex]);
+
+    const checkAnswer = (userAnswer) => {
+        if (userAnswer === currentNews.answer) {
+            setAnswerMessage(`정답! ${userAnswer ? "진실" : "거짓"}입니다!`);
+        } else {
+            setAnswerMessage(`오답! ${!userAnswer ? "진실" : "거짓"}입니다!`);
+        }
+        setIsAnswerShown(true);
+    };
+
+    const nextQuestion = () => {
+        if (currentNewsIndex < quizData.length - 1) {
+            setCurrentNewsIndex(currentNewsIndex + 1);
+            setIsAnswerShown(false);
+            setAnswerMessage('');
+        } else {
+            // 모든 문제를 마쳤을 때의 로직
+        }
     };
 
     const goToHome = () => {
@@ -19,38 +42,40 @@ const FakenewsQuiz = () => {
 
     return (
         <div>
-            {/* 헤더 및 소개 텍스트 */}
             <div className="fakenews-header">
                 <h1 onClick={goToHome} style={{ cursor: 'pointer' }}>🥝 QuizKiwi</h1>
                 <div className="fakenews-header-title">가짜뉴스 퀴즈</div>
             </div>
             <div className="fakenews-quiz-game-container">
-                {gameStarted ? (
-                    <>
-                        <div className="news-image">
-                            <img src={currentNews.imageUrl} alt="News Visual" width="500" height="500" />
+                <div className="news-image">
+                    <img src={currentNews.imageUrl} alt="News Visual" width="500" height="500" />
+                </div>
+                <div className="fakenews-quiz-box">
+                    <div className="fakenews-quiz-box-header">
+                        <div className="fakenews-controls-container">
+                            <span className="fakenews-control-button"></span>
+                            <span className="fakenews-control-button"></span>
+                            <span className="fakenews-control-button"></span>
                         </div>
-                        <div className="fakenews-quiz-box">
-                            <div className="fakenews-quiz-box-header">
-                                <div className="fakenews-controls-container">
-                                    <span className="fakenews-control-button"></span>
-                                    <span className="fakenews-control-button"></span>
-                                    <span className="fakenews-control-button"></span>
-                                </div>
+                    </div>
+                    <div className="news-question">
+                        <h2 className="news-title">{currentNews.title}</h2>
+                        <p className="news-body">{currentNews.body}</p>
+                        {!isAnswerShown ? (
+                            <div className="button-container">
+                                <button className="answer-button true-button" onClick={() => checkAnswer(true)}>진실</button>
+                                <button className="answer-button false-button" onClick={() => checkAnswer(false)}>거짓</button>
                             </div>
-                            <div className="news-question">
-          <h2 className="news-title">{currentNews.title}</h2>
-          <p className="news-body">{currentNews.body}</p>
-          <div className="button-container">
-          <button className="answer-button true-button">진실</button>
-          <button className="answer-button false-button">거짓</button>
-                            </div>
-                            </div>
-                            </div>
-                    </>
-                ) : (
-                    <button onClick={startGame} className="start-button">Start Game</button>
-                )}
+                        ) : (
+                            <div className="answer-feedback">{answerMessage}</div>
+                        )}
+                    </div>
+                    {isAnswerShown && (
+                        <button className="next-question-button" onClick={nextQuestion}>
+                            <FontAwesomeIcon icon={faArrowRight} /> 
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
     );
